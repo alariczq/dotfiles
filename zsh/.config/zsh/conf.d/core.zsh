a9ec67f8020d4736791259aa04478781dce1b400
vika <imzhongqi@gmail.com>
2026-05-18 15:15:50 +0800

    refactor(zsh): move FZF and EDITOR env from tools.zsh to core.zsh

--- zsh/.config/zsh/conf.d/core.zsh
+++ zsh/.config/zsh/conf.d/core.zsh
@@ -35,3 +35,31 @@ bindkey '^d'                delete-char
 bindkey '^o'                edit-command-line
 bindkey '^[q'               push-line-or-edit
 bindkey ' '                 magic-space
+
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_OPTS='--ansi
+--prompt="❯ "
+--pointer="▌ "
+--marker="✔ "
+--layout=reverse
+--info="inline: "
+--scrollbar=▊
+--color=fg:#c0caf5,\
+pointer:#9d79d6,\
+header:#738091,\
+prompt:#baa1e2,\
+border:#39506d,\
+scrollbar:#39506d,\
+gutter:#192330,\
+marker:#73daca,\
+separator:#39506d,\
+hl:#9d79d6,\
+info:#dc8ed9,\
+hl+:#9d79d6,\
+bg+:#29394f,\
+spinner:#baa1e2,\
+fg+:#cdcecf'
+export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --height=~40% --min-height=10 --preview='eza -1 --color=always --icons --group-directories-first {2..}'"
+
+(( $+commands[nvim] )) && EDITOR=nvim || EDITOR=vim
+export EDITOR
--- zsh/.config/zsh/conf.d/tools.zsh
+++ zsh/.config/zsh/conf.d/tools.zsh
@@ -17,17 +17,3 @@ _lazy 'eval "$(zoxide init zsh)"'
 _lazy '(( $+commands[atuin]  )) && eval "$(atuin init zsh --disable-up-arrow)"'
 _lazy '(( $+commands[direnv] )) && eval "$(direnv hook zsh)"'
 
-(( $+commands[nvim] )) && EDITOR=nvim || EDITOR=vim
-export EDITOR
-
-export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_DEFAULT_OPTS='
---ansi
---prompt="❯ "
---pointer="▌ "
---marker="✔ "
---layout=reverse
---info="inline: "
---scrollbar=▊
---color=fg:#c0caf5,pointer:#9d79d6,header:#738091,prompt:#baa1e2,border:#39506d,scrollbar:#39506d,gutter:#192330,marker:#73daca,separator:#39506d,hl:#9d79d6,info:#dc8ed9,hl+:#9d79d6,bg+:#29394f,spinner:#baa1e2,fg+:#cdcecf'
-export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --height=~40% --min-height=10 --preview='eza -1 --color=always --icons --group-directories-first {2..}'"
